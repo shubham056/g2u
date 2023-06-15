3569f5bf9528e7e1b4fe12a085dd0c8db540a379
Mintu Bisht <m.bisht0805@gmail.com>
2023-06-15 16:21:52 +0530

    added faq

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '@/components/_App/Header'
 import { NextSeo } from 'next-seo';
+import { apiBaseUrl, fetchApi } from "@/utils/fetchApi";
 
-const faq = () => {
+const faq = ({ faqData }) => {
   const SEO = {
     title: "Frequently Asked Questions | Games2U Mobile Entertainment",
     description: "Find answers to the most frequently asked question about Games2U, America's most trusted provider of mobile entertainment including video game trucks, laser tag equipment, human hamster balls, and more!",
@@ -54,54 +55,24 @@ const faq = () => {
       <div className="container-fluid" id="faqParent">
         <div className="row" id="faqContent">
           <div className="limited-width">
-            <div className="faq-container">
-              <h2>How much does it cost?</h2>
-              <p>Our prices are based on many factors including the games you select and the length of your event. So, while we can't quote a one-size-fits-all price, we can promise you that we can create an incredible experience to fit almost any budget. Call us today and find out for yourself!</p>
-            </div>
-            <div className="faq-container">
-              <h2>Is there an age limit?</h2>
-              <p>No. At Games2U we have more great games and then anyone and we can offer you activities appropriate for any age range.</p>
-            </div>
-            <div className="faq-container">
-              <h2>Do parents have to supervise?</h2>
-              <p>No. Our Games2U events are managed by our trained Game Coaches who are there for the safety of your participants and to make sure your event is a success!</p>
-            </div>
-            <div className="faq-container">
-              <h2>Do you need to plug in for electricity?</h2>
-              <p>Not usually. Our video game trucks are powered by on-board generators and other equipment is fully charged before your event. If we do require a power source for your event, you'll be told at the time of booking.</p>
-            </div>
-            <div className="faq-container">
-              <h2>How many players do your games accommodate? </h2>
-              <p>As many as you need! We're equally at home entertaining a party of 10 or servicing an event with hundreds of attendees. No matter the size or scope of your event, we have the activities to make it a success!</p>
-            </div>
-            <div className="faq-container">
-              <h2>Can I book a combination party featuring multiple games and equipment like laser tag, hamster balls and video game trucks?</h2>
-              <p>Of course. We offer multiple packages and add-ons to help you create an event they	ll never forget.</p>
-            </div>
-            <div className="faq-container">
-              <h2>What video games are available on your game trucks?</h2>
-              <p>We're gamers at heart and our current games are constantly changing. But you can rest assured that we'll show up with the best and newest games available.</p>
-            </div>
-            <div className="faq-container">
-              <h2>Can I choose the video games that are used on your game trucks?</h2>
-              <p>Our game coaches will be happy to discuss the games currently offered when they arrive for your event and we understand that parents may wish to only utilize certain games from our catalog.</p>
-            </div>
-            <div className="faq-container">
-              <h2>Are your events always outdoors?</h2>
-              <p>No. Most of our equipment can be used both indoors and outdoors with adequate space.</p>
-            </div>
-            <div className="faq-container">
-              <h2>Do we need a lot of space for games like Laser Tag? </h2>
-              <p>Not as much as you might think. We can set up in many front or backyards, cul-de-sacs or neighborhood parks or school yards. We're experts at turning your landscape into a challenging battlefield!</p>
-            </div>
-            <div className="faq-container">
-              <h2>Are the laser tag guns safe? Can they hurt a player's eyes? </h2>
-              <p>Games2U laser tag guns are perfectly safe and are not harmful to the eyes.</p>
-            </div>
-            <div className="faq-container">
-              <h2>What if it's bad weather? Do you have a cancelation policy?</h2>
-              <p>Games2U events happen rain or shine and we'll only cancel an event in the rare instance when we believe the conditions are not safe for the participants or our equipment. For more details, please see our Cancelation Policy.</p>
-            </div>
+            {
+              faqData
+                &&
+                (faqData.length > 0) ?
+                faqData.map(item => {
+                  return (
+                    <div className="faq-container">
+                      <h2>{item.question}</h2>
+                      <p>{item.answer}</p>
+                    </div>
+                  )
+                })
+                :
+                <div className='text-center'>
+                  <p>No records found!</p>
+                </div>
+            }
+
           </div>
         </div>
       </div>
@@ -111,3 +82,31 @@ const faq = () => {
 }
 
 export default faq
+
+export async function getStaticProps() {
+  try {
+    const payload = {
+      url: `${apiBaseUrl}/faq`, method: 'POST', data: { page_limit: 20, page_record: 1 }
+    }
+    const faqContent = await fetchApi(payload);
+    const faqData = faqContent.data.faq;
+    if (faqData && faqData.faq != undefined && faqData.faq == '') {
+      return {
+        notFound: true,
+        revalidate: 5,
+      };
+    } else {
+      return {
+        props: {
+          faqData
+        },
+        revalidate: 5, // In seconds
+      };
+    }
+  } catch (error) {
+    console.log('error in faq api call', error)
+    return {
+      notFound: true
+    };
+  }
+}
