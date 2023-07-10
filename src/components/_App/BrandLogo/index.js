8b70d5c40c04bb8e0028d21c015b8a7b24d1ca89
Mintu Bisht <m.bisht0805@gmail.com>
2023-07-10 16:10:14 +0530

    added

--- src/components/_App/BrandLogo/index.js
+++ src/components/_App/BrandLogo/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import dynamic from 'next/dynamic';
+import Image from 'next/image';
 const OwlCarousel = dynamic(import('react-owl-carousel'), { ssr: false });
 const options = {
     loop: true,
     nav: true,
-    navElement: 'div',
+    //navElement: 'div',
     startPosition: 0,
     navText:
         [
@@ -51,7 +52,10 @@ const BrandLogo = ({ investors }) => {
                         {
                             investors && investors.length > 0
                                 ?
-                                investors.map(item => <div className="as-seen-img" key={item.id}><img src={item.logo} alt={item.investor_name} /></div>)
+                                investors.map(item => <div className="as-seen-img" key={item.id}><Image src={item.logo} alt={item.investor_name} width={200} height={200} blurDataURL="data:..." placeholder="blur" style={{
+                                    width: '450px',
+                                    height: 'auto',
+                                  }} /></div>)
                                 :
                                 <div className="as-seen-img"> <p>No data found!</p></div>
                         }
