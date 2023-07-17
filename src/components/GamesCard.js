048a2cc46690c235b211fa3b1dcced6793a91014
Mintu Bisht <m.bisht0805@gmail.com>
2023-07-17 15:10:13 +0530

    added Next Image

--- src/components/GamesCard.js
+++ src/components/GamesCard.js
@@ -1,14 +1,16 @@
 import trimString from '@/utils/trimString'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const GamesCard = ({ dynamicClass, link, slug, imgSrc, icon, categoryName, caption }) => {
     return (
         <div className={dynamicClass} data-original="true">
             <Link href={`/${link}/game/${slug}`}>
                 <div className="box-heading video-game-theater" >
-                    <img src={imgSrc != '' ? imgSrc : "/assets/img/ico-video-theater-2x.png"} height={"100%"} />
+                    <Image src={imgSrc != '' ? imgSrc : "/assets/img/ico-video-theater-2x.png"} height={"500"} width={"500"} style={{ width: "100%", height: "100%", objectFit: 'cover' }} />
                 </div>
-                <div className="circle-img"><img src={icon != '' ? icon : "/assets/img/ico-video-theater-2x.png"} alt={slug} />
+                <div className="circle-img">
+                    <Image src={icon != '' ? icon : "/assets/img/ico-video-theater-2x.png"} width={500} height={500} alt={slug} />
                 </div>
                 <h3 className='singile-line'>{categoryName}</h3>
                 <p className='multiline-ellipsis games-sub-title'>{trimString(caption, 85)}</p>
--- src/components/_App/GamesList/index.js
+++ src/components/_App/GamesList/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import useGamesData from '@/states/stores/games-data';
+import Image from 'next/image';
 
 const GamesList = () => {
     const { zipcode, games, loading, error, updateGamesData } = useGamesData();
@@ -17,7 +18,7 @@ const GamesList = () => {
                                 <div className="col-md-3 col-sm-6 col-xs-12 col-ie-3" key={`game-list-${item.id}`}>
                                     <div className="ti-inline-block home-icon">
                                         <Link href={`/${games.affiliate.city.toLowerCase()}/game/${item.slug}`}>
-                                            <img src={item.icon_dark != '' ? item.icon_dark : "assets/img/ico-video-game-theater-blue-2x.png"} />
+                                            <Image src={item.icon_dark != '' ? item.icon_dark : "assets/img/ico-video-game-theater-blue-2x.png"} width={500} height={500} />
                                         </Link>
                                     </div>
                                     <div className="ti-inline-block home-game-name">
