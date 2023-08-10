a8be8f067b62874d7002d56b1be0556200ece303
Mintu Bisht <m.bisht0805@gmail.com>
2023-08-10 13:21:07 +0530

    added

--- src/components/_App/PlanYourParty/index.js
+++ src/components/_App/PlanYourParty/index.js
@@ -8,7 +8,7 @@ const PlanYourParty = ({ siteSettings, planningYourParty }) => {
                     <div className="col-md-6">
                         {planningYourParty && planningYourParty.page_name != '' ? <h2>{planningYourParty.page_name} <span className="force-break">1, 2, 3!</span></h2> : null}
 
-                        <div className="row ti-row-top">b 
+                        <div className="row ti-row-top">
                             {planningYourParty && <div className="col-lg-11 col-sm-10 col-xs-12 col-ie-10" dangerouslySetInnerHTML={{ __html: planningYourParty.content }}></div>}
 
                         </div>
