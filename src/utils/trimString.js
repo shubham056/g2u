640c1484098f43c110573a8cc160ba2d0a7eaaef
cwstechnology123 <monika.kumari@cwsinfotech.com>
2024-04-18 11:59:29 +0530

    init

--- src/utils/trimString.js
+++ src/utils/trimString.js
@@ -1,6 +1,6 @@
 const trimString = (string, limit) => {
     var dots = "...";
-    if (string.length > limit) {
+    if (string && string.length > limit) {
         // you can also use substr instead of substring
         string = string.substring(0, limit) + dots;
     }
