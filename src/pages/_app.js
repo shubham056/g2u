541d1083075145bc99185f192fcdddec9c9d22b5
cwstechnology123 <monika.kumari@cwsinfotech.com>
2024-07-10 20:00:05 +0530

    init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,12 @@
 import '@/styles/globals.css'
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
-import Layout from '../components/_App/Layout';
+//import Layout from '../components/_App/Layout';
 import axios from "axios";
 import { Analytics } from '@vercel/analytics/react';
 import SEO from '../../next-seo-config';
 import { DefaultSeo } from 'next-seo';
+import NextNProgress from 'nextjs-progressbar';
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL
 axios.defaults.headers.post['Content-Type'] = 'application/json';
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo {...SEO} />
+      <NextNProgress color="#D35E13" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} options={{ showSpinner: false }} />
       <Component {...pageProps} />
       <Analytics />
     </>
